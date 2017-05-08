34acfa87fc63cfdf138de32560f7c4b15981251a
Krisrinidhi <sk2423@njit.edu>
2017-05-08 17:56:33 -0400

    Update checkoff.php

--- controller/checkoff.php
+++ controller/checkoff.php
@@ -8,10 +8,10 @@ if (isset($_GET['Id']) && is_numeric($_GET['Id']))
 	$checkoff = $_GET['Id'];
 	$checkoff_query = "UPDATE todo SET Status='Complete' WHERE Id=$checkoff";
 	mysqli_query($db,$checkoff_query);
-	header("Location: home.php");
+	header("Location:home.php");
 }
 else
 {
-	header("Location: home.php");
+	header("Location:home.php");
 }
 ?>
