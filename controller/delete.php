47ba963b2ca704ad44fef81665b25031cf359ae8
Krisrinidhi <sk2423@njit.edu>
2017-05-08 17:56:57 -0400

    Update delete.php

--- controller/delete.php
+++ controller/delete.php
@@ -8,10 +8,10 @@ if (isset($_GET['Id']) && is_numeric($_GET['Id']))
 	$delete = $_GET['Id'];
 	$delete_query = "DELETE FROM todo WHERE Id= $delete";
 	mysqli_query($db,$delete_query);
-	header("Location: home.php");
+	header("Location:home.php");
 }
 else
 {
-	header("Location: home.php");
+	header("Location:home.php");
 }
 ?>
